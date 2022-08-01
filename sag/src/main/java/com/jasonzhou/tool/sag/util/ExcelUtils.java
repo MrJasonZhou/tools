694d183b731b36d83ad5b44591c9013ea83b8083
Jason Zhou <oldbug@gmail.com>
2022-08-01 11:53:26 +0900

    dev

--- sag/src/main/java/com/jasonzhou/tool/sag/util/ExcelUtils.java
+++ sag/src/main/java/com/jasonzhou/tool/sag/util/ExcelUtils.java
@@ -2,20 +2,17 @@
 package com.jasonzhou.tool.sag.util;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.format.CellFormatter;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellCopyPolicy;
@@ -38,23 +35,21 @@ import com.jasonzhou.tool.sag.excel.ValueFormatter;
 
 /**
  * Excelファイルを操作するUtil
- * 
+ *
  * @author Jason Zhou
  *
  */
 public class ExcelUtils {
-	
+
 	private static Map<Sheet, List<CellRangeAddress >> mapRange = new HashMap<>();
-	
+
 	private static Map<Workbook, Map<String, CellStyle>> cellLineStyles = new HashMap<>();
-	
+
 	private static DataFormatter df = new DataFormatter();
 
-	private static Map<String, ValueFormatter> vfMap = new HashMap<>();
-	
 	/**
 	 * シートの結合セル情報を取得し、キャシューさせる
-	 * 
+	 *
 	 * @param sheet	シート
 	 */
 	private static void loadCellRange(Sheet sheet) {
@@ -68,16 +63,16 @@ public class ExcelUtils {
 				CellRangeAddress range = sheet.getMergedRegion(i);
 				list.add(range);
 			}
-			list.sort((a, b) -> 
-				a.getFirstRow()> b.getFirstRow() ? 1 : (a.getFirstRow() == b.getFirstRow() ? Integer.compare(a.getFirstColumn(), b.getFirstColumn()):-1 ) );
+			list.sort((a, b) ->
+			a.getFirstRow()> b.getFirstRow() ? 1 : a.getFirstRow() == b.getFirstRow() ? Integer.compare(a.getFirstColumn(), b.getFirstColumn()):-1 );
 			mapRange.put(sheet, list);
 		}
-		
+
 	}
-	
+
 	/**
 	 * シートの行を削除する
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo	行番号
 	 */
@@ -88,17 +83,17 @@ public class ExcelUtils {
 
 	/**
 	 * シートの行をクリアする
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo	行番号
 	 */
 	public static void clearRow(Sheet sheet, int rowNo) {
 		sheet.removeRow(sheet.getRow(rowNo));
 	}
-	
+
 	/**
 	 * 指定された位置の結合範囲を取得する
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo	行番号
 	 * @param colNo　列番号
@@ -107,24 +102,24 @@ public class ExcelUtils {
 	private static CellRangeAddress getCellRange(Sheet sheet, int rowNo, int colNo) {
 		loadCellRange(sheet);
 		for (CellRangeAddress range : mapRange.get(sheet)) {
-			if (rowNo >= range.getFirstRow() 
-					&& rowNo <= range.getLastRow() 
-					&& colNo >= range.getFirstColumn() 
+			if (rowNo >= range.getFirstRow()
+					&& rowNo <= range.getLastRow()
+					&& colNo >= range.getFirstColumn()
 					&& colNo <= range.getLastColumn()) {
 				return range;
 			} else {
-				if (rowNo > range.getLastRow() || (rowNo == range.getLastRow() && colNo > range.getLastColumn())) {
+				if (rowNo > range.getLastRow() || rowNo == range.getLastRow() && colNo > range.getLastColumn()) {
 					break;
 				}
 			}
-			
+
 		}
 		return null;
 	}
 
 	/**
 	 * 指定行と列のセルを取得する（結合された場合、最初のセル以外、nullを返す）
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo	指定行
 	 * @param colNo	指定列
@@ -133,14 +128,14 @@ public class ExcelUtils {
 	public static Cell getCell(Sheet sheet, int rowNo, int colNo) {
 		return getCell(sheet, rowNo, colNo, false);
 	}
-	
+
 	/**
 	 * 指定行と列のセルを取得する
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo	指定行
 	 * @param colNo	指定列
-	 * @param asFirstCell 結合された場合、結合エリアの最初のセルを返す 
+	 * @param asFirstCell 結合された場合、結合エリアの最初のセルを返す
 	 * @return	セル情報
 	 */
 	public static Cell getCell(Sheet sheet, int rowNo, int colNo, boolean asFirstCell) {
@@ -149,7 +144,7 @@ public class ExcelUtils {
 			//最初のセル以外
 			if (!asFirstCell && (rowNo != range.getFirstRow() || colNo != range.getFirstColumn())) {
 				return null;
-			}  
+			}
 			rowNo = range.getFirstRow();
 			colNo = range.getFirstColumn();
 		}
@@ -166,106 +161,63 @@ public class ExcelUtils {
 
 	/**
 	 * 設定ファイルを読み込む
-	 * 
-	 * @param fileName	ファイル名
+	 *
+	 * @param is	設定ファイルのストリーム
 	 * @return	Excelファイル
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static Workbook  load(String fileName) throws  IOException {
-		return load(new File(fileName));
+	public static Workbook load(InputStream is) throws  IOException {
+		return new SagWorkBook(is);
 	}
 
 	/**
-	 * 設定ファイルを読み込む
-	 * 
-	 * @param fileName	ファイル名
-	 * @return	Excelファイル
-	 * @throws IOException 
-	 */
-	public static Workbook  load(InputStream is, String fileName) throws  IOException {
-        if (fileName.toLowerCase().endsWith("xlsx") || fileName.toLowerCase().endsWith("xlsm")) {
-            return new XSSFWorkbook(is);
-        } else if (fileName.toLowerCase().endsWith("xls") ) {
-            return new HSSFWorkbook(is);
-        } else {
-        	is.close();
-            throw new IOException("ファイルフォーマットエラー");
-        }
-     }
-	
-	/**
-	 * 設定ファイルを読み込む
-	 * 
-	 * @param is	設定ファイルのストリーム
-	 * @return	Excelファイル
-	 * @throws IOException 
-	 */
-	public static Workbook  load(InputStream is) throws  IOException {
-		try{
-			return load(is, "dummy.xlsx");
-		} catch (Exception e) {
-			return load(is, "dummy.xls");
-		}
-     }
-	
-	/**
-	 * ファイルを読み込む
-	 * 
-	 * @param file	ファイル
-	 * @return	Excelファイル
-	 * @throws IOException 
-	 */
-	public static Workbook load(File file) throws  IOException {
-		return load(new FileInputStream(file), file.getName());
-	}	
-	/**
 	 * Excelファイルを作成する
-	 * 
+	 *
 	 * @return	Excelファイル
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static Workbook  create() throws  IOException {
-		
-        Workbook workbook  =  new XSSFWorkbook();
+
+		Workbook workbook  =  new XSSFWorkbook();
 		return workbook;
-        
+
 	}
-	
+
 	/**
 	 * 指定するフォーマットのCellスタイルを取得する
-	 * 
+	 *
 	 * @param workbook		Excelファイル
 	 * @param cellFormat		セルフォーマット
 	 * @return	指定するフォーマットのCellスタイル
 	 */
 	private static CellStyle getCellStyle(Workbook workbook, CellFormatter...  cellFormats) {
-		
+
 		Map<String, CellStyle> map = cellLineStyles.get(workbook);
 		List<String> formatList = new ArrayList<>();
 		for (CellFormatter cf : cellFormats) {
 			formatList.add(cf.toString());
 		}
 		String key = StringUtils.join(formatList, ":");
-		
+
 		if (map == null) {
 			map = new HashMap<>();
 			cellLineStyles.put(workbook, map);
 		}
-        CellStyle cellStyle = map.get(key);
-        if (cellStyle == null) {
-        	cellStyle = workbook.createCellStyle();
-        	for (CellFormatter cf : cellFormats) {
-            	cf.format(cellStyle);
-        	}
-    		map.put(key, cellStyle);
-        }
-        return cellStyle;
+		CellStyle cellStyle = map.get(key);
+		if (cellStyle == null) {
+			cellStyle = workbook.createCellStyle();
+			for (CellFormatter cf : cellFormats) {
+				cf.format(cellStyle);
+			}
+			map.put(key, cellStyle);
+		}
+		return cellStyle;
 	}
-	
+
 
 	/**
 	 * Excelファイルを保存する
-	 * 
+	 *
 	 * @param workbook	Workbook
 	 * @param fileName		ファイル名
 	 * @throws Exception
@@ -283,10 +235,10 @@ public class ExcelUtils {
 			workbook.write(out);
 		}
 	}
-	
+
 	/**
 	 * セルのコメント情報を取得する
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo		行番号
 	 * @param colNo		列番号
@@ -302,7 +254,7 @@ public class ExcelUtils {
 
 	/**
 	 * セルのコメント情報を取得する
-	 * 
+	 *
 	 * @param cell	セル
 	 * @return	コメント情報
 	 */
@@ -323,7 +275,7 @@ public class ExcelUtils {
 
 	/**
 	 * セルのテキスト情報を取得する
-	 * 
+	 *
 	 * @param cell	セル
 	 * @return	セルのテキスト情報
 	 */
@@ -331,12 +283,12 @@ public class ExcelUtils {
 		if (cell == null) {
 			return "";
 		}
-	     return df.formatCellValue(cell);
+		return df.formatCellValue(cell);
 	}
 
 	/**
 	 * セルのテキスト情報を取得する
-	 * 
+	 *
 	 * @param row	行
 	 * @param colNo	列番号
 	 * @return	セルのテキスト情報
@@ -347,10 +299,10 @@ public class ExcelUtils {
 		}
 		return getCellText(row.getCell(colNo));
 	}
-	
+
 	/**
 	 * セルのテキスト情報を取得する
-	 * 
+	 *
 	 * @param sheet シート
 	 * @param rowNo	行番号
 	 * @param colNo	列番号
@@ -360,10 +312,10 @@ public class ExcelUtils {
 		Cell cell = getCell(sheet, rowNo, colNo, false);
 		return getCellText(cell);
 	}
-	
+
 	/**
 	 * セルのテキスト情報を設定する
-	 * 
+	 *
 	 * @param cell	セル
 	 * @param text	テキスト情報
 	 */
@@ -373,10 +325,10 @@ public class ExcelUtils {
 		}
 		cell.setCellValue(text);;
 	}
-	
+
 	/**
 	 * セルのテキスト情報を設定する
-	 * 
+	 *
 	 * @param cell	セル
 	 * @param text	テキスト情報
 	 */
@@ -384,10 +336,10 @@ public class ExcelUtils {
 		ValueFormatter vf = null;
 		setCellValue(cell, vf, values);
 	}
-	
+
 	/**
 	 * セルのテキスト情報を設定する
-	 * 
+	 *
 	 * @param cell	セル
 	 * @param vf 	値フォーマット
 	 * @param text	テキスト情報
@@ -424,13 +376,13 @@ public class ExcelUtils {
 			if (value instanceof BigDecimal) {
 				BigDecimal bd = (BigDecimal) value;
 				cell.setCellValue(bd.doubleValue());
-			}							
+			}
 		}
-	}		
-	
+	}
+
 	/**
 	 * セルをフォーマットする
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo	行番号
 	 * @param colNo	カラム番号
@@ -445,10 +397,10 @@ public class ExcelUtils {
 		CellStyle style = getCellStyle(sheet.getWorkbook(), cellFormat);
 		cell.setCellStyle(style);
 	}
-	
+
 	/**
 	 * シートを取得する
-	 * 
+	 *
 	 * @param workbook		Excelファイル
 	 * @param sheetName		シート名
 	 * @return	ソート
@@ -460,10 +412,10 @@ public class ExcelUtils {
 		}
 		return sheet;
 	}
-	
+
 	/**
 	 * シートのタブ色を設定する
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param color	タブ色
 	 */
@@ -474,10 +426,10 @@ public class ExcelUtils {
 			xSheet.setTabColor(new XSSFColor(rgb,null));
 		}
 	}
-	
+
 	/**
 	 * シートの位置を調整する
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param pos	位置
 	 */
@@ -485,7 +437,7 @@ public class ExcelUtils {
 		Workbook book = sheet.getWorkbook();
 		book.setSheetOrder(sheet.getSheetName(), pos);
 	}
-	
+
 	/**
 	 * POIで行をコピーする処理
 	 * @param book ワークブック
@@ -513,110 +465,110 @@ public class ExcelUtils {
 	 * @param destRowNo コピー先の行インデックス
 	 */
 	private static void _copyRow(Workbook book, Sheet sheet, int srcRowNo, int destRowNo) {
-	  Row newRow = sheet.getRow(destRowNo);
-	  Row srcRow = sheet.getRow(srcRowNo);
+		Row newRow = sheet.getRow(destRowNo);
+		Row srcRow = sheet.getRow(srcRowNo);
 
-	  if (newRow != null) {
-	    //コピー先に行が既に存在する場合、１行下にずらす
-	    sheet.shiftRows(destRowNo, sheet.getLastRowNum(), 1);
-	    newRow = sheet.createRow(destRowNo);
-	  } else {
-	    //存在しない場合は作成
-	    newRow = sheet.createRow(destRowNo);
-	  }
+		if (newRow != null) {
+			//コピー先に行が既に存在する場合、１行下にずらす
+			sheet.shiftRows(destRowNo, sheet.getLastRowNum(), 1);
+			newRow = sheet.createRow(destRowNo);
+		} else {
+			//存在しない場合は作成
+			newRow = sheet.createRow(destRowNo);
+		}
 
-	  // セルの型、スタイル、値などをすべてコピーする
-	  for (int i = 0; i < srcRow.getLastCellNum(); i++) {
-	    Cell oldCell = srcRow.getCell(i);
-	    // コピー元の行が存在しない場合、処理を中断
-	    if (oldCell == null) {
-	      continue;
-	    }
-	    Cell newCell = newRow.createCell(i);
+		// セルの型、スタイル、値などをすべてコピーする
+		for (int i = 0; i < srcRow.getLastCellNum(); i++) {
+			Cell oldCell = srcRow.getCell(i);
+			// コピー元の行が存在しない場合、処理を中断
+			if (oldCell == null) {
+				continue;
+			}
+			Cell newCell = newRow.createCell(i);
 
 
-	    //スタイルのコピー
-	    CellStyle newCellStyle = book.createCellStyle();
-	    newCellStyle.cloneStyleFrom(oldCell.getCellStyle());
-	    newCell.setCellStyle(newCellStyle);
+			//スタイルのコピー
+			CellStyle newCellStyle = book.createCellStyle();
+			newCellStyle.cloneStyleFrom(oldCell.getCellStyle());
+			newCell.setCellStyle(newCellStyle);
 
-	    //コメントのコピー
-	    if (oldCell.getCellComment() != null) {
-	      newCell.setCellComment(oldCell.getCellComment());
-	    }
+			//コメントのコピー
+			if (oldCell.getCellComment() != null) {
+				newCell.setCellComment(oldCell.getCellComment());
+			}
 
-	    //ハイパーリンクのコピー
-	    if (oldCell.getHyperlink() != null) {
-	      newCell.setHyperlink(oldCell.getHyperlink());
-	    }
+			//ハイパーリンクのコピー
+			if (oldCell.getHyperlink() != null) {
+				newCell.setHyperlink(oldCell.getHyperlink());
+			}
 
-	    //セル型のコピー
-	    //newCell.setCellType(oldCell.getCellType());
+			//セル型のコピー
+			//newCell.setCellType(oldCell.getCellType());
 
-	    //セルの値をコピー
-	    switch (oldCell.getCellType()) {
-	    case BLANK:
-	      newCell.setCellValue(oldCell.getStringCellValue());
-	      break;
-	    case BOOLEAN:
-	      newCell.setCellValue(oldCell.getBooleanCellValue());
-	      break;
-	    case ERROR:
-	      newCell.setCellErrorValue(oldCell.getErrorCellValue());
-	      break;
-	    case FORMULA:
-	      newCell.setCellFormula(oldCell.getCellFormula());
-	      break;
-	    case NUMERIC:
-	      newCell.setCellValue(oldCell.getNumericCellValue());
-	      break;
-	    case STRING:
-	      newCell.setCellValue(oldCell.getRichStringCellValue());
-	      break;
-	     default:
-	    	 newCell.setBlank();
-	    	 break;
-	    }
-	  }
+			//セルの値をコピー
+			switch (oldCell.getCellType()) {
+			case BLANK:
+				newCell.setCellValue(oldCell.getStringCellValue());
+				break;
+			case BOOLEAN:
+				newCell.setCellValue(oldCell.getBooleanCellValue());
+				break;
+			case ERROR:
+				newCell.setCellErrorValue(oldCell.getErrorCellValue());
+				break;
+			case FORMULA:
+				newCell.setCellFormula(oldCell.getCellFormula());
+				break;
+			case NUMERIC:
+				newCell.setCellValue(oldCell.getNumericCellValue());
+				break;
+			case STRING:
+				newCell.setCellValue(oldCell.getRichStringCellValue());
+				break;
+			default:
+				newCell.setBlank();
+				break;
+			}
+		}
 
-	  //セル結合のコピー
-	  for (int i = 0; i < sheet.getNumMergedRegions(); i++) {
-	    CellRangeAddress cellRangeAddress = sheet.getMergedRegion(i);
-	    if (cellRangeAddress.getFirstRow() == srcRow.getRowNum()) {
-	      CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.getRowNum(),
-	          (newRow.getRowNum() + (cellRangeAddress.getLastRow() - cellRangeAddress.getFirstRow())),
-	          cellRangeAddress.getFirstColumn(), cellRangeAddress.getLastColumn());
-	      sheet.addMergedRegion(newCellRangeAddress);
-	    }
-	  }
+		//セル結合のコピー
+		for (int i = 0; i < sheet.getNumMergedRegions(); i++) {
+			CellRangeAddress cellRangeAddress = sheet.getMergedRegion(i);
+			if (cellRangeAddress.getFirstRow() == srcRow.getRowNum()) {
+				CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.getRowNum(),
+						newRow.getRowNum() + cellRangeAddress.getLastRow() - cellRangeAddress.getFirstRow(),
+						cellRangeAddress.getFirstColumn(), cellRangeAddress.getLastColumn());
+				sheet.addMergedRegion(newCellRangeAddress);
+			}
+		}
 	}
-	
-//	/**
-//	 * ValueFormatterを取得する
-//	 * 
-//	 * @param format	フォーマット
-//	 * @return　ValueFormatterのインスタンス
-//	 */
-//	public static ValueFormatter getValueFormatter(String format) {
-//		ValueFormatter vf = vfMap.get(format);
-//		if (vf != null) {
-//			return vf;
-//		}
-//		
-//		String[] arr = ConvertUtil.split(format, ":");
-//		String name = arr[0];
-//		vf =SpringUtil.getApplicationContext().getBean(name, ValueFormatter.class);
-//		if (arr.length > 1) {
-//			String[] params = ConvertUtil.split(arr[1], ",");
-//			vf.setParams(params);
-//		}
-//		vfMap.put(format, vf);
-//		return vf;
-//	}
-	
+
+	//	/**
+	//	 * ValueFormatterを取得する
+	//	 *
+	//	 * @param format	フォーマット
+	//	 * @return　ValueFormatterのインスタンス
+	//	 */
+	//	public static ValueFormatter getValueFormatter(String format) {
+	//		ValueFormatter vf = vfMap.get(format);
+	//		if (vf != null) {
+	//			return vf;
+	//		}
+	//
+	//		String[] arr = ConvertUtil.split(format, ":");
+	//		String name = arr[0];
+	//		vf =SpringUtil.getApplicationContext().getBean(name, ValueFormatter.class);
+	//		if (arr.length > 1) {
+	//			String[] params = ConvertUtil.split(arr[1], ",");
+	//			vf.setParams(params);
+	//		}
+	//		vfMap.put(format, vf);
+	//		return vf;
+	//	}
+
 	/**
 	 * 指定されたコメント内容からコメント→セルテキストのマップを取得する
-	 * 
+	 *
 	 * @param row	指定された行
 	 * @param comments	コメントの配列
 	 * @return	マップ（キー＝コメント、値＝セルテキスト）
@@ -637,12 +589,12 @@ public class ExcelUtils {
 			}
 		}
 		return map;
-		
+
 	}
 
 	/**
 	 * 指定されたコメント内容からコメント→カラム番号のマップを取得する
-	 * 
+	 *
 	 * @param row	指定された行
 	 * @param comments	コメントの配列
 	 * @return	マップ（キー＝コメント、値＝カラム番号）
@@ -663,10 +615,10 @@ public class ExcelUtils {
 		}
 		return map;
 	}
-	
+
 	/**
 	 * シートの指定する位置に内容はあるかを判断する
-	 * 
+	 *
 	 * @param sheet	シート
 	 * @param rowNo	行番号
 	 * @param colNo	列番号
@@ -683,6 +635,40 @@ public class ExcelUtils {
 		}
 		String text = getCellText(cell);
 		return StringUtils.isBlank(text);
+	}
+
+	/**
+	 * Excelブックをクローズ時イベント
+	 *
+	 * @param book	Excelブック
+	 */
+	static void onClose(Workbook book) {
+		List<Sheet> list = new ArrayList<>();
+		mapRange.forEach(
+				(sheet,range) -> {if (sheet.getWorkbook() == book) {list.add(sheet);}});
+		list.forEach( sheet -> mapRange.remove(sheet));
+		cellLineStyles.remove(book);
+	}
+}
+
+
+/**
+ *　Sag専用なExcelブック
+ *
+ * @author Jason Zhou
+ *
+ */
+class SagWorkBook extends XSSFWorkbook {
+
+	public SagWorkBook(InputStream is) throws IOException {
+		super(is);
+	}
+
+	@Override
+	public void close() throws IOException {
+		ExcelUtils.onClose(this);
+		super.close();
+
 	}
 
 }
